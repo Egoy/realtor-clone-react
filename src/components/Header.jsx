4c814a754ea6261e5a93ad8d93a021d06079807f
Egoy <cangcoder@gmail.com>
2022-12-17 16:13:51 +0800

    created pages for sign in, sign up, forgot password, and OAuth component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ export default function Header() {
     }
   return (
     <div className='bg-white border-b shadow-sm sticky top-0 z-50'>
-        <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
+        <header className='flex justify-between items-center px-3 max-w-6xl mx-auto lg:p-5'>
             <div>
                 <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="Realtor Logo" className='h-5 cursor-pointer' onClick={() => navigate('/')}/>
             </div>
