baa336d8462a3cd4b7295bfa4c269917c55e7f35
Egoy <cangcoder@gmail.com>
2022-12-22 09:41:48 +0800

    Fixed profile link not updating when logged in

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import { useLocation, useNavigate } from 'react-router'
 
 export default function Header() {
     const [pageState, setPageState] = useState('Sign in')
+    const [pageLink, setPageLink] = useState('/signin')
     const location = useLocation()
     const navigate = useNavigate()
     const auth = getAuth()
@@ -12,8 +13,10 @@ export default function Header() {
         onAuthStateChanged(auth, (user)=> {
             if (user) {
                 setPageState('Profile')
+                setPageLink('/profile')
             } else {
                 setPageState('Sign in')
+                setPageLink('/sign-in')
             }
         })
     }, [auth])
@@ -34,7 +37,7 @@ export default function Header() {
                 <ul className='flex space-x-10'>
                     <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute('/') && 'text-black border-b-red-500'}`} onClick={() => navigate('/')}>Home</li>
                     <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute('/offers') && 'text-black border-b-red-500'}`} onClick={() => navigate('/offers')}>Offers</li>
-                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${(pathMatchRoute('/sign-in') || pathMatchRoute('/profile')) && 'text-black border-b-red-500'}`} onClick={() => navigate('/sign-in')}>{pageState}</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${(pathMatchRoute('/sign-in') || pathMatchRoute('/profile')) && 'text-black border-b-red-500'}`} onClick={() => navigate(pageLink)}>{pageState}</li>
                 </ul>
             </div>
         </header>
